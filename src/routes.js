86b4c65f12a6b2e78017302e3bef709d3351c38d
Rodrigo Lourenço <rsilvape@gmail.com>
2020-03-31 14:02:39 -0300

    Created first model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rodrigo Lourenço',
+    email: 'rsilvape@gmail.com',
+    password_hash: '12312312312',
+  });
+  return res.json(user);
 });
 
 module.exports = routes;
